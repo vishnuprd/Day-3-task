8e4758ee32b18d38085bf78ad1c32278482833d7
vishnuprd <141515800+vishnuprd@users.noreply.github.com>
2023-09-25 17:56:47 +0530

    json and rest countries

---
+++ script1.js
@@ -0,0 +1,19 @@
+//create a XMLHTTPRequest 
+var request = new XMLHttpRequest();
+//2nd step: API URL
+//.open method
+//2 parameters
+request.open("GET","https://restcountries.com/v3.1/all");
+//3rd step : establishing the bridge
+request.send();
+//4th step : once the data sucessfully received from server
+//onload is an event
+//when we use :  once the data / server is showing 200 sataus code
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    for(let i = 0 ; i< result.length ; i++){
+        console.log(result[i].name);
+        console.log(result[i].region);
+        console.log(result[i].subregion); 
+        console.log(result[i].population); 
+}
